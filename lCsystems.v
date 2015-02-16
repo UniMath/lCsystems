06a2ee9a205300f6a2f46dfad1da55e9d8c5af5e
Vladimir Voevodsky <vladimir@ias.edu>
2015-02-15 20:02:15 -0500

    The type of l-C-systems is defined. The work on the constructions needed for the foramlization of the l-C-system - to - l-B-system construction started.

--- lCsystems.v
+++ lCsystems.v
@@ -5,6 +5,10 @@ started Dec. 4, 2014, continued Jan. 22, 2015, Feb. 11, 2015
 We refer below to the paper "Subsystems and regular quotients of C-systems"
 by V. Voevodsky as "Csubsystems".
 
+The definition of an lC-system given below does not require that the types of objects or morphisms
+of the underlying precategory form a set. It also does not require the
+proporties of the identity morphisms but does require associativity. 
+
 *)
 
 Require Export Foundations.hlevel2.hnat .
@@ -16,6 +20,8 @@ Unset Automatic Introduction.
 Local Notation "a --> b" := (precategory_morphisms a b)(at level 50).
 
 Local Notation "f ;; g" := (compose f g)(at level 50).
+
+ 
 
 Definition mor_from { C : precategory_ob_mor } ( X : C ) :=
   total2 ( fun A : C => X --> A ) .
@@ -57,6 +63,10 @@ as a precategory whose objects for a pointed hSet-ltower with the additional str
 the canonical projections pX : X --> ft X . *)
 
 
+
+(** **** l-tower precategories *)
+
+
 Definition ltower_precat := total2 ( fun C : precategory => ltower_str C ) . 
 
 Definition ltower_precat_to_ltower ( CC : ltower_precat ) : ltower :=
@@ -66,58 +76,81 @@ Coercion ltower_precat_to_ltower : ltower_precat >-> ltower .
 Definition ltower_precat_pr1 : ltower_precat -> precategory := pr1 .
 Coercion ltower_precat_pr1 : ltower_precat >-> precategory .
 
-Definition ltower_and_p_precat :=
+Definition ltower_precat_and_p :=
   total2 ( fun CC : ltower_precat  => forall X : CC , X --> ft X ) .
 
-Definition ltower_and_p_precat_pr1 : ltower_and_p_precat -> ltower_precat := pr1 . 
-Coercion ltower_and_p_precat_pr1 : ltower_and_p_precat >-> ltower_precat . 
+Definition ltower_precat_and_p_pr1 : ltower_precat_and_p -> ltower_precat := pr1 . 
+Coercion ltower_precat_and_p_pr1 : ltower_precat_and_p >-> ltower_precat . 
                                                           
-Definition pX { CC : ltower_and_p_precat } ( X : CC ) : X --> ft X := pr2 CC X .
+Definition pX { CC : ltower_precat_and_p } ( X : CC ) : X --> ft X := pr2 CC X .
 
-Definition pltower_and_p_precat :=
-  total2 ( fun CC : ltower_and_p_precat => ispointed CC ) .
 
-Definition pltower_and_p_precat_pr1 : pltower_and_p_precat ->
-                                             ltower_and_p_precat := pr1 .
-Coercion pltower_and_p_precat_pr1 : pltower_and_p_precat >->
-                                             ltower_and_p_precat.
+(** **** Some constructions *)
 
-Definition pltower_and_p_precat_to_pltower : pltower_and_p_precat -> pltower :=
+
+Definition ftf { CC : ltower_precat_and_p } { X Y : CC } ( f : X --> Y ) : X --> ft Y :=
+  f ;; pX Y . 
+
+Definition Ob_tilde_rel { CC : ltower_precat_and_p  } ( X : CC ) :=
+  total2 ( fun r : ft X --> X => r ;; ( pX X ) = identity ( ft X ) ) .
+
+Definition Ob_tilde_rel_to_mor_to { CC : ltower_precat_and_p } ( X : CC ) ( r : Ob_tilde_rel X ) :
+  mor_to X := mor_to_constr ( pr1 r ) .
+Coercion Ob_tilde_rel_to_mor_to : Ob_tilde_rel >-> mor_to . 
+
+Definition Ob_tilde_rel_to_mor_from { CC : ltower_precat_and_p  } ( X : CC ) ( r : Ob_tilde_rel X ) :
+  mor_from ( ft X ) := mor_from_constr ( pr1 r ) .
+Coercion Ob_tilde_rel_to_mor_from : Ob_tilde_rel >-> mor_from .
+
+
+(** **** Pointed ltower precategories *)
+
+
+Definition pltower_precat_and_p :=
+  total2 ( fun CC : ltower_precat_and_p => ispointed CC ) .
+
+Definition pltower_precat_and_p_pr1 : pltower_precat_and_p ->
+                                             ltower_precat_and_p := pr1 .
+Coercion pltower_precat_and_p_pr1 : pltower_precat_and_p >->
+                                             ltower_precat_and_p.
+
+Definition pltower_precat_and_p_to_pltower : pltower_precat_and_p -> pltower :=
   fun X => pltower_constr ( pr2 X ) . 
-Coercion pltower_and_p_precat_to_pltower : pltower_and_p_precat >-> pltower . 
+Coercion pltower_precat_and_p_to_pltower : pltower_precat_and_p >-> pltower .
 
 
-Definition q_data_type ( CC : ltower_and_p_precat ) := 
+(** **** l-C0-system data *)
+
+
+Definition q_data_type ( CC : ltower_precat_and_p ) := 
   forall ( X Y : CC ) ( gt0 : ll X > 0 ) ( f : Y --> ft X ) , mor_to X . 
 Identity Coercion from_q_data_type : q_data_type >-> Funclass .  
 
 
-Definition lC0system_data := total2 ( fun CC : pltower_and_p_precat => q_data_type CC ) .
+Definition lC0system_data := total2 ( fun CC : pltower_precat_and_p => q_data_type CC ) .
 
-Definition lC0system_data_pr1 : lC0system_data -> pltower_and_p_precat  := pr1 .
-Coercion lC0system_data_pr1 : lC0system_data >-> pltower_and_p_precat .
+Definition lC0system_data_pr1 : lC0system_data -> pltower_precat_and_p  := pr1 .
+Coercion lC0system_data_pr1 : lC0system_data >-> pltower_precat_and_p .
 
-Definition q_data { CC : lC0system_data }  
+
+Definition q_of_f { CC : lC0system_data }  
            { X Y : CC } ( gt0 : ll X > 0 ) ( f : Y --> ft X ) : mor_to X :=
   pr2 CC _ _ gt0 f . 
 
 Definition f_star { CC : lC0system_data }  
            { X Y : CC } ( gt0 : ll X > 0 ) ( f : Y --> ft X ) : CC := 
-  pr1 ( q_data gt0 f ) . 
-
-Definition q_of_f { CC : lC0system_data }
-           { X Y : CC } ( gt0 : ll X > 0 ) ( f : Y --> ft X ) : f_star gt0 f --> X :=
-  pr2 ( q_data gt0 f ) .
+  pr1 ( q_of_f gt0 f ) . 
 
 
+(** **** Properties on l-C0-system data 
 
+that later become axioms of an lC0-system. The numbering of the conditions below is according to 
+the Csubsystems paper.
 
-(** The numbering of the conditions below is according to the Csubsystems paper
-
-The axioms 1-3 are consequences of the definition of a pointed l-tower (pltower) *)
+The conditions 1-3 are consequences of the definition of a pointed l-tower (pltower) *)
            
 
-Definition C0ax4_type ( CC : pltower_and_p_precat ) :=
+Definition C0ax4_type ( CC : pltower_precat_and_p ) :=
   forall X : CC , iscontr ( X --> cntr CC ) . 
 
 Definition C0ax5a_type ( CC : lC0system_data ) :=
@@ -126,14 +159,14 @@ Definition C0ax5a_type ( CC : lC0system_data ) :=
 Definition C0ax5b_type ( CC : lC0system_data ) :=
   forall ( X Y : CC ) ( gt0 : ll X > 0 ) ( f : Y --> ft X ) , ft ( f_star gt0 f ) = Y .
 
-(** A construction needed to formulate further conditions on the C0-systm data. *)
+(** A construction needed to formulate further properties of the C0-system data. *)
 
 Definition C0ax5b_mor { CC : lC0system_data } ( ax5b : C0ax5b_type CC )
            { X Y : CC } ( gt0 : ll X > 0 ) ( f : Y --> ft X ) :
   ft ( f_star gt0 f ) --> Y :=
   transportf ( fun A : CC => ft ( f_star gt0 f ) --> A ) ( ax5b X Y gt0 f ) ( identity _ ) . 
 
-(** The definition of conditions continues *)
+(** The description of properties continues *)
 
 Definition C0ax5c_type { CC : lC0system_data }
            ( ax5b : C0ax5b_type CC ) := 
@@ -142,17 +175,17 @@ Definition C0ax5c_type { CC : lC0system_data }
 
 Definition C0ax6_type ( CC : lC0system_data ) :=
   forall ( X : CC ) ( gt0 : ll X > 0 ) ,
-    q_data gt0 ( identity ( ft X ) ) = mor_to_constr ( identity X ) . 
+    q_of_f gt0 ( identity ( ft X ) ) = mor_to_constr ( identity X ) . 
 
 Definition C0ax7_type { CC : lC0system_data } 
   ( ax5a : C0ax5a_type CC ) ( ax5b : C0ax5b_type CC ) :=
   forall ( X Y Z : CC ) ( gt0 : ll X > 0 ) ( f : Y --> ft X ) ( g : Z --> ft ( f_star gt0 f ) ) ,
     mor_to_constr ( ( q_of_f ( ax5a _ _ gt0 f ) g ) ;; ( q_of_f gt0 f ) ) =
-    q_data gt0 ( g ;; ( ( C0ax5b_mor ax5b gt0 f ) ;; f ) ) . 
+    q_of_f gt0 ( g ;; ( ( C0ax5b_mor ax5b gt0 f ) ;; f ) ) . 
 
 
 
-(** **** The type of lC0-systems *)
+(** **** The type of l-C0-systems *)
 
 
 Definition lC0system :=
@@ -187,7 +220,7 @@ Definition C0ax5c { CC : lC0system }
 
 
 Definition C0ax6 { CC : lC0system } { X : CC } ( gt0 : ll X > 0 ) :
-  q_data gt0 ( identity ( ft X ) ) = mor_to_constr ( identity X ) :=
+  q_of_f gt0 ( identity ( ft X ) ) = mor_to_constr ( identity X ) :=
   pr1 ( pr2 ( pr2 ( pr2 CC ))) X gt0 .
 
 Definition C0ax6a { CC : lC0system } { X : CC } ( gt0 : ll X > 0 ) :
@@ -197,7 +230,7 @@ Definition C0ax6a { CC : lC0system } { X : CC } ( gt0 : ll X > 0 ) :
 Definition C0ax7 { CC : lC0system }
            { X Y Z : CC } ( gt0 : ll X > 0 ) ( f : Y --> ft X ) ( g : Z --> ft ( f_star gt0 f ) ) :
   mor_to_constr ( ( q_of_f ( C0ax5a gt0 f ) g ) ;; ( q_of_f gt0 f ) ) =
-  q_data gt0 ( g ;; ( ( C0emor gt0 f ) ;; f ) ) :=
+  q_of_f gt0 ( g ;; ( ( C0emor gt0 f ) ;; f ) ) :=
   pr2 ( pr2 ( pr2 ( pr2 CC ))) X Y Z gt0 f g . 
 
 Definition C0ax7a { CC : lC0system }
@@ -208,53 +241,51 @@ Definition C0ax7a { CC : lC0system }
 
 
 
-(** **** Some constructions for lC0-systems *)
-
-
-Definition ftf { CC : ltower_and_p_precat } { X Y : CC } ( f : X --> Y ) : X --> ft Y :=
-  f ;; pX Y . 
-
-Definition Ob_tilde { CC : ltower_and_p_precat  } ( X : CC ) :=
-  total2 ( fun r : ft X --> X => r ;; ( pX X ) = identity ( ft X ) ) .
-
-Definition Ob_tilde_to_mor_to { CC : ltower_and_p_precat } ( X : CC ) ( r : Ob_tilde X ) : mor_to X :=
-  mor_to_constr ( pr1 r ) .
-Coercion Ob_tilde_to_mor_to : Ob_tilde >-> mor_to . 
-
-Definition Ob_tilde_to_mor_from { CC : ltower_and_p_precat  } ( X : CC ) ( r : Ob_tilde X ) :
-  mor_from ( ft X ) := mor_from_constr ( pr1 r ) .
-Coercion Ob_tilde_to_mor_from : Ob_tilde >-> mor_from . 
-  
-
-
-(** *** The lC-systems *)
 
 
 
-(** **** The required structures/conditions on lC0systems 
-
-The structures/conditions on lC0-systems that will become axioms of lCsystems. *)
+(** *** The l-C-systems *)
 
 
+(** **** l-C-system data *) 
 
-Definition sf_type ( CC : lC0system ) :=
-  forall ( Y X : CC ) ( gt0 : ll X > 0 ) ( f : Y --> X ) , Ob_tilde ( f_star gt0 ( ftf f ) ) .
+Definition sf_type ( CC : lC0system_data ) :=
+  forall ( Y X : CC ) ( gt0 : ll X > 0 ) ( f : Y --> X ) , Ob_tilde_rel ( f_star gt0 ( ftf f ) ) .
 
-(* Definition sf_ax1_type { CC : lC0system } ( sf : sf_type CC ) :=
+(* ??? Need canonical structures to productively use the following concept???
+
+Definition lCsystem_data := total2 ( fun CC : lC0system_data => sf_type CC ) .
+
+Definition lCsystem_data_constr { CC : lC0system_data } ( sf0 : sf_type CC ) : lCsystem_data :=
+  tpair _ _ sf0 . 
+
+Definition lCsystem_data_pr1 : lCsystem_data -> lC0system_data := pr1 .
+Coercion lCsystem_data_pr1 : lCsystem_data >-> lC0system_data .
+
+Definition sf { CC : lCsystem_data } { Y X : CC } ( gt0 : ll X > 0 ) ( f : Y --> X ) :
+  Ob_tilde_rel ( f_star gt0 ( ftf f ) ) :=
+  pr2 CC Y X gt0 f . 
+
+*)
+
+
+
+
+(** **** Properties on l-C-system data 
+
+that later become axioms of l-C-systems. *)
+
+
+Definition sf_ax1_type { CC : lC0system } ( sf0 : sf_type CC ) :=
   forall ( Y X : CC ) ( gt0 : ll X > 0 ) ( f : Y --> X ) ,
-    sf Y X gt0 f ;; pX ( f_star gt0 ( ftf f ) ) =
-    transportb ( fun A => Y --> A ) ( C0ax5b gt0 ( ftf f ) ) ( identity Y ) . *)
+    ( C0emor gt0 ( ftf f ) ) ;; f = ( sf0 _ _ gt0 f ) ;; ( q_of_f gt0 ( ftf f ) ) .
 
-Definition sf_ax1_type { CC : lC0system } ( sf : sf_type CC ) :=
-  forall ( Y X : CC ) ( gt0 : ll X > 0 ) ( f : Y --> X ) ,
-    ( C0emor gt0 ( ftf f ) ) ;; f = ( sf Y X gt0 f ) ;; ( q_of_f gt0 ( ftf f ) ) .
-
-Lemma sf_ax2_type_l1 { CC : lC0system } ( sf : sf_type CC )
+Lemma sf_ax2_type_l1 { CC : lC0system } ( sf0 : sf_type CC )
       { Y Y' U : CC } ( gt0 : ll U > 0 )
       ( g : Y' --> ft U ) ( f : Y --> f_star gt0 g ) :
   f_star (C0ax5a gt0 g) (ftf f) = f_star gt0 (ftf (f ;; q_of_f gt0 g)) .
 Proof.
-  intros.
+  intros. 
   assert ( int1 : f_star (C0ax5a gt0 g) (ftf f) =
                   f_star gt0 ( ( ftf f ) ;; ( ( C0emor gt0 g ) ;; g ) ) ) .
   apply C0ax7a.
@@ -281,38 +312,72 @@ Defined.
 Definition sf_ax2_type { CC : lC0system } ( sf : sf_type CC ) :=
   forall ( Y Y' U : CC ) ( gt0 : ll U > 0 )
          ( g : Y' --> ft U ) ( f : Y --> f_star gt0 g ) ,
-     transportf Ob_tilde  (sf_ax2_type_l1 sf gt0 g f ) ( sf Y _ ( C0ax5a gt0 g ) f ) =
+     transportf Ob_tilde_rel  (sf_ax2_type_l1 sf gt0 g f ) ( sf Y _ ( C0ax5a gt0 g ) f ) =
      sf Y _ gt0 ( f ;; q_of_f gt0 g ) .  
 
 
-(** **** The definition of the type of lCsystems *)
-
+(** **** The definition of the type of l-C-systems *)
 
 
 Definition lCsystem :=
   total2 ( fun CC : lC0system =>
-             total2 ( fun sf : sf_type CC =>
-                        dirprod ( sf_ax1_type sf ) ( sf_ax2_type sf ) ) ) .
+             total2 ( fun sf0 : sf_type CC =>
+                        dirprod ( sf_ax1_type sf0 ) ( sf_ax2_type sf0 ) ) ) .
 
 Definition lCsystem_pr1 : lCsystem -> lC0system := pr1 .
 Coercion lCsystem_pr1 : lCsystem >-> lC0system .
 
-Definition sf { CC : lCsystem } { Y X : CC } ( gt0 : ll X > 0 ) ( f : Y --> X ) :
-  Ob_tilde ( f_star gt0 ( ftf f ) ) :=
-  pr1 ( pr2 CC ) Y X gt0 f . 
+(* Definition lCsystem_to_lCsystem_data ( CC : lCsystem ) : lCsystem_data :=
+  @lCsystem_data_constr CC ( pr1 ( pr2 CC ) ) .
+Coercion lCsystem_to_LCsystem_data : lCsystem >-> lCsystem_data . *)
 
+Definition sf { CC : lCsystem } { Y X : CC } ( gt0 : ll X > 0 ) ( f : Y --> X ) :
+  Ob_tilde_rel ( f_star gt0 ( ftf f ) ) := pr1 ( pr2 CC ) _ _ gt0 f . 
+  
 Definition sf_ax1 { CC : lCsystem } { Y X : CC } ( gt0 : ll X > 0 ) ( f : Y --> X ) :
   ( C0emor gt0 ( ftf f ) ) ;; f = ( sf gt0 f ) ;; ( q_of_f gt0 ( ftf f ) ) :=
   pr1 ( pr2 ( pr2 CC ) ) Y X gt0 f . 
 
 Definition sf_ax2 { CC : lCsystem } { Y Y' U : CC } ( gt0 : ll U > 0 )
            ( g : Y' --> ft U ) ( f : Y --> f_star gt0 g ) :
-  transportf Ob_tilde  (sf_ax2_type_l1 ( @sf CC ) gt0 g f ) ( sf ( C0ax5a gt0 g ) f ) =
+  transportf Ob_tilde_rel  (sf_ax2_type_l1 ( @sf CC ) gt0 g f ) ( sf ( C0ax5a gt0 g ) f ) =
   sf gt0 ( f ;; q_of_f gt0 g ) :=
   pr2 ( pr2 ( pr2 CC ) ) Y Y' U gt0 g f .
 
 
                                 
+(** *** Operations fn_star and qn_of_f *)
+
+Definition qn { CC : lC0system_data } { Y A : CC } ( f : Y --> A ) ( n : nat ) 
+           { X : CC } ( eq : ftn n X = A )  : mor_to X .
+Proof.
+  intros until n . 
+  induction n as [ | n IHn ] .
+  intros . 
+  change _ with ( X = A ) in eq . 
+  rewrite eq . apply ( mor_to_constr f ) . 
+
+  intros .
+  
+  assert ( eq' : ftn n ( ft X ) = A ) . rewrite ftn_ft . apply eq . 
+
+  set ( qn := IHn ( ft X ) eq' ) .
+  destruct ( natgehchoice _ _ ( natgehn0 ( ll X ) ) ) as [ gt0 | eq0 ] .  
+  
+  apply ( @q_of_f CC _ _ gt0 qn ) .
+
+  assert ( eq'' : ft X = X ) . apply ( ftX_eq_X eq0 ) .
+  rewrite <- eq'' . 
+  apply qn . 
+
+Defined.
+
+Definition fn_star { CC : lC0system_data } { Y A : CC } ( f : Y --> A ) ( n : nat ) 
+           { X : CC } ( eq : ftn n X = A ) : CC := pr1 ( qn f n eq ) .
+
+
+(* Definition fun_star_s { CC : lCsystem } { Y A : CC } ( f : Y --> A ) ( n : nat ) 
+           { X : CC } ( eq : ftn n X = A ) ( r : Ob_tilde *)
 
 
 
